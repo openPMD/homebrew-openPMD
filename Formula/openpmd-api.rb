75ff905632e26feb181f58fa3638ed4e9b364d55
Axel Huebl <axel.huebl@plasma.ninja>
2020-01-29 16:29:12 -0800

    Initial Commit: openPMD-api 0.10.3
    - [ ] `brew test openpmd-api`

---
+++ Formula/openpmd-api.rb
@@ -0,0 +1,69 @@
+class OpenpmdApi < Formula
+  desc "C++ & Python API for Scientific I/O with openPMD"
+  homepage "https://openpmd-api.readthedocs.io"
+  url "https://github.com/openPMD/openPMD-api/archive/0.10.3-alpha.tar.gz"
+  sha256 "4d3677b6e3b674510ec6376978e6fd125ab830c467cf41b982a982e3e242f805"
+  head "https://github.com/openPMD/openPMD-api.git", :branch => "dev"
+
+  depends_on "cmake" => :build
+  # pkg-config (add for downstream convenience?)
+  # adios (no package)
+  depends_on "adios2"
+  depends_on "catch2"
+  # "hdf5" (no MPI package)
+  depends_on "openpmd/openpmd/hdf5-mpi"
+  # mpark-variant (no package)
+  depends_on "mpi4py"
+  depends_on "nlohmann-json"
+  depends_on "numpy"
+  depends_on "open-mpi"
+  depends_on "pybind11"
+  depends_on "python"
+
+  def install
+    args = std_cmake_args + %W[
+      -DopenPMD_USE_MPI=ON
+      -DopenPMD_USE_HDF5=ON
+      -DopenPMD_USE_ADIOS1=OFF
+      -DopenPMD_USE_ADIOS2=ON
+      -DopenPMD_USE_PYTHON=ON
+      -DopenPMD_USE_INTERNAL_PYBIND11=OFF
+      -DopenPMD_USE_INTERNAL_CATCH=OFF
+      -DPYTHON_EXECUTABLE:FILEPATH=#{Formula["python"].opt_bin}/python3
+      -DBUILD_TESTING=OFF
+      -DBUILD_EXAMPLES=OFF
+    ]
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+    end
+
+    (pkgshare/"examples").install "examples/5_write_parallel.cpp"
+    (pkgshare/"examples").install "examples/5_write_parallel.py"
+
+    # environment setups
+    # TODO: PYTHONPATH?
+    # bin.env_script_all_files("#{libexec}/lib/pkgconfig", :PKG_CONFIG_PATH => ENV["PKG_CONFIG_PATH"])
+  end
+
+  test do
+    system "mpic++", "-std=c++11",
+           (pkgshare/"examples/5_write_parallel.cpp"),
+           "-I#{opt_include}",
+           "-lopenPMD"
+    system "mpiexec",
+           "-n", "2",
+           "./a.out"
+    assert_predicate testpath/"../samples/5_parallel_write.h5", :exist?
+
+    system "#{Formula["python"].opt_bin}/python3",
+           "-c", "import openpmd_api"
+
+    system "mpiexec",
+           "-n", "2",
+           "#{Formula["python"].opt_bin}/python3",
+           "-m", "mpi4py",
+           (pkgshare/"examples/5_write_parallel.py")
+    assert_predicate testpath/"../samples/5_parallel_write_py.h5", :exist?
+  end
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# openPMD Formulas for Homebrew
+
+**THIS REPO IS A WORK-IN-PROGRESS! DO NOT USE YET!**
+
+This [Homebrew](http://brew.sh) formula installs provides recipes to build [openPMD](https://www.openpmd.org) libraries and tools on your MacOS or Linux system.
+
+TL;DR: Just execute `brew tap openpmd/openpmd` and `brew install openpmd-api`.
+
+
+## Provided Packages
+
+- [openpmd-api](https://github.com/openPMD/openPMD-api)
+
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
+
+
+## Tests
+
+With `brew test openpmd-api` you can check if the formula is working properly. Note the formula needs to be installed before it can be tested.
