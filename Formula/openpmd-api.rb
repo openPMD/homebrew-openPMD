f59d236bc7001086b14d5f662897ad8725411158
Axel Huebl <axel.huebl@plasma.ninja>
2021-01-05 23:14:24 -0800

    Prefer Python_EXECUTABLE
    Showed to be more robust in other package managers

--- Formula/openpmd-api.rb
+++ Formula/openpmd-api.rb
@@ -28,7 +28,7 @@ class OpenpmdApi < Formula
       -DopenPMD_USE_PYTHON=ON
       -DopenPMD_USE_INTERNAL_PYBIND11=OFF
       -DopenPMD_USE_INTERNAL_CATCH=OFF
-      -DPython_ROOT_DIR:FILEPATH=#{Formula["python"].prefix}
+      -DPython_EXECUTABLE:FILEPATH=#{Formula["python"].opt_bin}/python3
       -DBUILD_TESTING=OFF
       -DBUILD_EXAMPLES=OFF
     ]
